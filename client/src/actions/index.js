aeb04aa9fd8b61db9b7bf61f97d75e9adeb70189
Eric <chene40@mcmaster.ca>
2022-05-28 14:38:30 -0400

    changed BrowserRouter to Router to manually configure history object

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
     DELETE_STREAM,
     EDIT_STREAM
 } from "./types"
+import history from '../history'
 
 export const signIn = (userId) => {
     return {
@@ -27,6 +28,8 @@ export const createStream = formValues => async (dispatch, getState) => {
     const { userId } = getState().auth
     const response = await streams.post('/streams', { ...formValues, userId } )
     dispatch( { type: CREATE_STREAM, payload: response.data } )
+    // programmatically navigate the user around
+    history.push('/')           // path that we want user to go to
 }
 
 export const fetchStreams = () => async dispatch => {
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 import StreamCreate from './streams/StreamCreate'
 import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
 import Header from './Header'
+import history from '../history'
 
 // CLIENT ID: 817113998350-esqssm6u4cqkdcoga7opq6mj70nt88k7.apps.googleusercontent.com
 
 export default function App(){
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList}/>
@@ -21,7 +22,7 @@ export default function App(){
                     <Route path="/streams/delete" exact component={StreamDelete}/>
                     <Route path="/streams/show" exact component={StreamShow}/>
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     )
 }
